f1de3ba77735b65574d6be46b36ab47a17126508
shankarsubhedar <shankarsubhedar120@gmail.com>
2024-08-01 13:27:34 +0530

    bubble sort, rect

---
+++ area_of_rect.c
@@ -0,0 +1,21 @@
+    #include<stdio.h>
+    int main(){
+       int day=2;
+       switch(day){
+              case1:
+              printf("monday");
+              break;
+              case2:
+              printf("tuesday");
+              break;
+       }
+              dafault:
+              printf("value not match");
+                return o;
+    }
+       
+       
+              
+       
+          
+
---
+++ array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int num[]={25,30,40,60};
+    for(int i=0;i<4;i++){
+        printf("%d\n",num[i]);
+}
+return 0;
+}
+
+
+
+
+
